889f1d642032b361dcd7ebc22445174cde396fa0
Andrija Mitrovic <andrijamitrovic11@gmail.com>
2026-05-10 01:25:42 +0200

    feat: added databse migrations for JobApplication table

---
+++ backend/JobScraper.Repository/Migrations/20260509232406_AddJobApplications.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobScraper.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobApplications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_JobPostings_Url",
+                table: "JobPostings");
+
+            migrationBuilder.CreateTable(
+                name: "JobApplications",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    JobPostingId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    AppliedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    InterviewAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    RejectedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    AppliedHereBefore = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", maxLength: 2000, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobApplications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobApplications_JobPostings_JobPostingId",
+                        column: x => x.JobPostingId,
+                        principalTable: "JobPostings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplications_JobPostingId",
+                table: "JobApplications",
+                column: "JobPostingId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobApplications");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_Url",
+                table: "JobPostings",
+                column: "Url",
+                unique: true);
+        }
+    }
+}
